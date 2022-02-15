87f6d8fd11bac07e1e63030284ba044f09695d6d
bornaparo <bornaparo@gmail.com>
2022-02-16 00:44:26 +0100

    first commit

---
+++ urs_zavrsni_projekt/lcd_functions.h
@@ -0,0 +1,376 @@
+#define F_CPU 7372800UL
+
+#include <avr/cpufunc.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+// Colors
+#define LBLUE 0x963D
+#define WHITE 0xFFFF
+#define BLACK 0x0000
+#define GREEN 0xc72b
+#define RED   0xD369
+#define CYAN  0x1AAE
+
+// Screen dimensions
+#define MAX_X 240
+#define MAX_Y 320
+
+// Pinout
+#define LCD_DATA_H PORTA // data pins DB8-DB15
+#define LCD_DATA_L PORTD // data pins DB0-DB7
+
+#define LCD_RS    PC0 // changing between commands and data
+#define LCD_WR    PC1 // write data
+#define LCD_RD    PC6 // read data
+#define LCD_CS    PC7 // chip select
+#define LCD_RESET PB1 // lcd reset
+
+#define T_CLK PB7 // touch controller clock
+#define T_CS  PB4 // chip select
+#define T_DIN PB5 // sending commands or data to touch part of screen, x and y coordinates
+#define T_DO  PB6 // receiving data from touch part of screen
+#define T_IRQ PB0 // interrupt, 1 if the screen is being touched
+#define TOUCH_PORT PORTB
+
+// RS definitions
+#define CMD 0  // command
+#define DATA 1 // data
+
+
+static const unsigned char font[37][5] = { //bilo [29] bez ovih zadnjih 8 brojeva jer su 1 i 2 vec bili
+	{0x7E, 0x11, 0x11, 0x11, 0x7E}, // 41 A
+	{0x7F, 0x49, 0x49, 0x49, 0x36}, // 42 B
+	{0x3E, 0x41, 0x41, 0x41, 0x22}, // 43 C
+	{0x7F, 0x41, 0x41, 0x22, 0x1C}, // 44 D
+	{0x7F, 0x49, 0x49, 0x49, 0x41}, // 45 E
+	{0x7F, 0x09, 0x09, 0x09, 0x01}, // 46 F
+	{0x3E, 0x41, 0x49, 0x49, 0x7A}, // 47 G
+	{0x7F, 0x08, 0x08, 0x08, 0x7F}, // 48 H
+	{0x00, 0x41, 0x7F, 0x41, 0x00}, // 49 I
+	{0x20, 0x40, 0x41, 0x3F, 0x01}, // 4a J
+	{0x7F, 0x08, 0x14, 0x22, 0x41}, // 4b K
+	{0x7F, 0x40, 0x40, 0x40, 0x40}, // 4c L
+	{0x7F, 0x02, 0x0C, 0x02, 0x7F}, // 4d M
+	{0x7F, 0x04, 0x08, 0x10, 0x7F}, // 4e N
+	{0x3E, 0x41, 0x41, 0x41, 0x3E}, // 4f O
+	{0x7F, 0x09, 0x09, 0x09, 0x06}, // 50 P
+	{0x3E, 0x41, 0x51, 0x21, 0x5E}, // 51 Q
+	{0x7F, 0x09, 0x19, 0x29, 0x46}, // 52 R
+	{0x46, 0x49, 0x49, 0x49, 0x31}, // 53 S
+	{0x01, 0x01, 0x7F, 0x01, 0x01}, // 54 T
+	{0x3F, 0x40, 0x40, 0x40, 0x3F}, // 55 U
+	{0x1F, 0x20, 0x40, 0x20, 0x1F}, // 56 V
+	{0x3F, 0x40, 0x38, 0x40, 0x3F}, // 57 W
+	{0x63, 0x14, 0x08, 0x14, 0x63}, // 58 X
+	{0x07, 0x08, 0x70, 0x08, 0x07}, // 59 Y
+	{0x61, 0x51, 0x49, 0x45, 0x43}, // 5a Z
+	{0x00, 0x00, 0x00, 0x00, 0x00}, // 20 space
+	
+	{0x3E, 0x51, 0x49, 0x45, 0x3E}, //0x30 0
+	
+	{0x00, 0x42, 0x7F, 0x40, 0x00}, // 31 1
+	{0x42, 0x61, 0x51, 0x49, 0x46},  // 32 2
+
+	{0x21, 0x41, 0x45, 0x4B, 0x31}, // 3
+	{0x18, 0x14, 0x12, 0x7F, 0x10}, // 4
+	{0x27, 0x45, 0x45, 0x45, 0x39}, // 5
+	{0x3C, 0x4A, 0x49, 0x49, 0x30}, // 6
+	{0x03, 0x71, 0x09, 0x05, 0x03}, // 7
+	{0x36, 0x49, 0x49, 0x49, 0x36}, // 8
+	{0x06, 0x49, 0x49, 0x29, 0x1E}, // 9
+};
+
+uint8_t get_bit(uint8_t reg, uint8_t offset) {
+	return (reg >> offset) & 1;
+}
+
+// touch part starts working
+void TFT_start() {
+	TOUCH_PORT |= _BV(T_CS) | _BV(T_CLK) | _BV(T_DIN);
+}
+
+// writes commands to touch
+void TFT_touch_write(uint8_t num) {
+	TOUCH_PORT &= ~_BV(T_CLK);
+	for (uint8_t i = 0; i < 8; i++) {
+		if (get_bit(num, 7 - i)) {
+			TOUCH_PORT |= _BV(T_DIN);
+			} else {
+			TOUCH_PORT &= ~_BV(T_DIN);
+		}
+		TOUCH_PORT &= ~_BV(T_CLK);
+		TOUCH_PORT |= _BV(T_CLK);
+	}
+}
+
+// reads data from ADC on touch part of the screen (coordiates)
+uint16_t TFT_touch_read() {
+	uint16_t value = 0;
+	for (uint8_t i = 0; i < 12; i++) {
+		value <<= 1;
+		TOUCH_PORT |= _BV(T_CLK);            // high signal on T_CLK
+		TOUCH_PORT &= ~_BV(T_CLK);           // low signal on T_CLK, initialization of data transfer
+		value += get_bit(PINB, T_DO);   // touch has 12-bit ADC, counting 0-12, taking one by one bit from T_DO,
+	}
+
+	return value;
+}
+
+// sending commands to screen
+void TFT_write(uint16_t val, uint8_t rs) {
+	if (rs) {                   // rs == 1 - data
+		PORTC |= _BV(LCD_RS);
+		} else {                    // rs == 0 - command
+		PORTC &= ~_BV(LCD_RS);
+	}
+	PORTC &= ~_BV(LCD_CS);
+	LCD_DATA_H = val >> 8;
+	LCD_DATA_L = val;
+	PORTC |= _BV(LCD_WR);
+	PORTC &= ~_BV(LCD_WR);
+	PORTC |= _BV(LCD_CS);
+}
+
+// sending specified command and value to memory
+void TFT_write_pair(uint16_t cmd, uint16_t data) {
+	TFT_write(cmd, CMD);
+	TFT_write(data, DATA);
+}
+
+// coordinates that define where elements will be drawn
+void TFT_set_address(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2) {
+	TFT_write_pair(0x0044, (x2 << 8) + x1);
+	TFT_write_pair(0x0045, y1);
+	TFT_write_pair(0x0046, y2);
+	TFT_write_pair(0x004e, x1);
+	TFT_write_pair(0x004f, y1);
+	TFT_write(0x0022, CMD);
+}
+
+void TFT_init(void) {
+	DDRA = 0xff ;
+	DDRD = 0xff;
+	DDRC |= _BV(LCD_RS) | _BV(LCD_WR) | _BV(LCD_RD) | _BV(LCD_CS); //0xff;
+	DDRB = ~(_BV(T_DO) | _BV(T_IRQ)); // pins for receiving data
+
+	// initializing lcd configuration
+	DDRB |= _BV(LCD_RESET) | _BV(PB2); //pb2 je LED (tj backlight)
+	PORTB |= _BV(LCD_RESET) | _BV(PB2);
+	_delay_ms(5);
+	PORTB &= ~_BV(LCD_RESET);
+	_delay_ms(10);
+	PORTB |= _BV(LCD_RESET);
+	PORTC |= _BV(LCD_CS);
+	PORTC |= _BV(LCD_RD);
+	PORTC &= ~_BV(LCD_WR);
+	_delay_ms(20);
+
+	TFT_write_pair(0x0000, 0x0001); _delay_ms(1);
+	TFT_write_pair(0x0003, 0xA8A4); _delay_ms(1);
+	TFT_write_pair(0x000C, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x000D, 0x080C); _delay_ms(1);
+	TFT_write_pair(0x000E, 0x2B00); _delay_ms(1);
+	TFT_write_pair(0x001E, 0x00B0); _delay_ms(1);
+	TFT_write_pair(0x0001, 0x2B3F); _delay_ms(1);
+	TFT_write_pair(0x0002, 0x0600); _delay_ms(1);
+	TFT_write_pair(0x0010, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0011, 0x6070); _delay_ms(1);
+	TFT_write_pair(0x0005, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0006, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0016, 0xEF1C); _delay_ms(1);
+	TFT_write_pair(0x0017, 0x0003); _delay_ms(1);
+	TFT_write_pair(0x0007, 0x0233); _delay_ms(1);
+	TFT_write_pair(0x000B, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x000F, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0041, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0042, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0048, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0049, 0x013F); _delay_ms(1);
+	TFT_write_pair(0x004A, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x004B, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0044, 0xEF00); _delay_ms(1);
+	TFT_write_pair(0x0045, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0046, 0x013F); _delay_ms(1);
+	TFT_write_pair(0x0030, 0x0707); _delay_ms(1);
+	TFT_write_pair(0x0031, 0x0204); _delay_ms(1);
+	TFT_write_pair(0x0032, 0x0204); _delay_ms(1);
+	TFT_write_pair(0x0033, 0x0502); _delay_ms(1);
+	TFT_write_pair(0x0034, 0x0507); _delay_ms(1);
+	TFT_write_pair(0x0035, 0x0204); _delay_ms(1);
+	TFT_write_pair(0x0036, 0x0204); _delay_ms(1);
+	TFT_write_pair(0x0037, 0x0502); _delay_ms(1);
+	TFT_write_pair(0x003A, 0x0302); _delay_ms(1);
+	TFT_write_pair(0x003B, 0x0302); _delay_ms(1);
+	TFT_write_pair(0x0023, 0x0000); _delay_ms(1);
+	TFT_write_pair(0x0024, 0x0000); _delay_ms(1);
+
+	TFT_write_pair(0x004f, 0);
+	TFT_write_pair(0x004e, 0);
+	TFT_write(0x0022, CMD);
+}
+
+// setting cursor to a specific position
+void TFT_set_cursor(uint16_t x, uint16_t y) {
+	TFT_write_pair(0x004E, x);
+	TFT_write_pair(0x004F, MAX_Y - y);
+	TFT_write(0x0022, CMD);
+}
+
+// reading x and y coordinates from touch part of screen
+void read_touch_coords(uint16_t *TP_X, uint16_t *TP_Y) {
+	_delay_ms(1);
+
+	TOUCH_PORT &= ~_BV(T_CS);
+
+	TFT_touch_write(0x90); // sending command to touch part of screen to write y coordinate
+
+	_delay_ms(1);
+	TOUCH_PORT |= _BV(T_CLK);  _NOP(); _NOP(); _NOP(); _NOP();
+	TOUCH_PORT &= ~_BV(T_CLK); _NOP(); _NOP(); _NOP(); _NOP();
+	*TP_Y = (TFT_touch_read() - 80) / 6;
+
+	TFT_touch_write(0xD0); // sendng command to touch part of screen to write x coordinate
+	TOUCH_PORT |= _BV(T_CLK);  _NOP(); _NOP(); _NOP(); _NOP();
+	TOUCH_PORT &= ~_BV(T_CLK); _NOP(); _NOP(); _NOP(); _NOP();
+	*TP_X = (TFT_touch_read() - 80) / 8;
+
+	TOUCH_PORT |= _BV(T_CS);
+}
+
+// fill the screen with the specied color
+void set_background_color(uint16_t color) {
+	TFT_set_address(0, 0, 239, 319);
+
+	for (uint16_t i = 0; i < 320; i++) {
+		for (uint8_t j = 0; j < 240; j++) {
+			TFT_write(color, DATA);
+		}
+	}
+}
+
+// setting a color of a pixel at the specified position
+void draw_pixel(uint16_t x, uint16_t y, uint16_t color) {
+	PORTC &= ~_BV(LCD_CS);
+	TFT_set_cursor(x, y);
+	TFT_write(color, DATA);
+	PORTC |= _BV(LCD_CS);
+}
+
+// setting a color of a pixel at the specified position for a letter
+void draw_font_pixel(uint16_t x, uint16_t y, uint16_t color, uint8_t pixel_size) {
+	for(uint8_t i = 0; i < pixel_size; i++) {
+		for(uint8_t j = 0; j < pixel_size; j++) {
+			draw_pixel(x + i, y + j, color);
+		}
+	}
+}
+
+// setting a color to the pixels needed to write the specified character
+void print_char(uint16_t x, uint16_t y, uint8_t font_size, uint16_t color, uint16_t back_color, uint8_t val) {
+	for (uint8_t i = 0x00; i < 0x05; i++) {
+		uint8_t value = font[val][i];
+		for (uint8_t j = 0x00; j < 0x08; j++) {
+			if ((value >> j) & 0x01) {
+				draw_font_pixel(x + j * font_size, y, color, font_size);
+				} else {
+				draw_font_pixel(x + j * font_size, y, back_color, font_size);
+			}
+		}
+		y += font_size;
+	}
+}
+
+// setting a color to the pixels needed to write the specified string
+void print_string(uint16_t x, uint16_t y, uint8_t font_size, uint16_t color, uint16_t back_color, const char *ch) {
+	uint8_t cnt = 0;
+
+	do {
+		if (ch[cnt] == ' ') {
+			print_char(x + font_size, y, font_size, color, back_color, 26);
+			} else if (ch[cnt] == '0') {
+			print_char(x + font_size, y, font_size, color, back_color, 27);
+			} else if (ch[cnt] == '1') {
+			print_char(x + font_size, y, font_size, color, back_color, 28);
+			} else if (ch[cnt] == '2') {
+			print_char(x + font_size, y, font_size, color, back_color, 29);
+			} else if (ch[cnt] == '3') {
+			print_char(x + font_size, y, font_size, color, back_color, 30);
+			} else if (ch[cnt] == '4') {
+			print_char(x + font_size, y, font_size, color, back_color, 31);
+			} else if (ch[cnt] == '5') {
+			print_char(x + font_size, y, font_size, color, back_color, 32);
+			} else if (ch[cnt] == '6') {
+			print_char(x + font_size, y, font_size, color, back_color, 33);
+			} else if (ch[cnt] == '7') {
+			print_char(x + font_size, y, font_size, color, back_color, 34);
+			} else if (ch[cnt] == '8') {
+			print_char(x + font_size, y, font_size, color, back_color, 35);
+			} else if (ch[cnt] == '9') {
+			print_char(x + font_size, y, font_size, color, back_color, 36);
+			} else {
+			print_char(x + font_size, y, font_size, color, back_color, ch[cnt] - 'A');
+		}
+		cnt++;
+		y += 0x05 * font_size + 0x01;
+	} while(ch[cnt] != '\0');
+}
+
+// setting a color to the pixels in a horizontal line
+void draw_h_line(uint16_t x1, uint16_t y1, uint16_t y2, uint16_t color) { //zapravo x1, y1, x2
+	for (; y1 < y2; y1++) {
+		draw_pixel(x1, y1, color);
+	}
+}
+
+// setting a color to the pixels in a vetical line
+void draw_v_line(uint16_t y1, uint16_t x1, uint16_t x2, uint16_t color) { //zapravo x1, y1, y2
+	for (; x1 < x2; x1++) {
+		draw_pixel(x1, y1, color);
+	}
+}
+
+// setting a color to the pixels in two diagonal lines
+void draw_cross(uint16_t x, uint16_t y, uint16_t d, uint16_t color) {
+	for (uint8_t i = 0; i < d; i++) {
+		draw_pixel(x + i, y + i, color);
+		draw_pixel(x + i, d - i + y, color);
+	}
+}
+
+// setting a color to the pixels in circle
+void draw_circle(uint16_t x0, uint16_t y0, uint16_t r, uint16_t color) {
+	x0 += r;
+	y0 += r;
+
+	int16_t x = -r, y = 0, err = 2 - 2 * r, e2;
+	do {
+		draw_pixel(x0 - x, y0 + y, color);
+		draw_pixel(x0 + x, y0 + y, color);
+		draw_pixel(x0 + x, y0 - y, color);
+		draw_pixel(x0 - x, y0 - y, color);
+
+		e2 = err;
+		if (e2 <= y) {
+			err += ++y * 2 + 1;
+			if (-x == y && e2 <= x)
+			e2 = 0;
+		}
+		if (e2 > x) {
+			err += ++x * 2 + 1;
+		}
+	} while (x <= 0);
+}
+
+// setting a color to the pixels of a rectangle
+void draw_rectangle(uint16_t x, uint16_t y, uint16_t dx, uint16_t dy, uint16_t color) {
+	draw_h_line(x, y, y + dy, color);
+	draw_h_line(x + dx, y, y + dy, color);
+	draw_v_line(y, x, x + dx, color);
+	draw_v_line(y + dy, x, x + dx, color);
+}
+
+// check if the screen is being touched
+uint8_t check_touch(uint16_t TP_X, uint16_t TP_Y, uint16_t x, uint16_t y, uint16_t dx, uint16_t dy) {
+	return TP_Y >= y && TP_Y <= y + dy && TP_X >= x && TP_X <= x + dx;
+}
---
+++ urs_zavrsni_projekt/main.cpp
@@ -0,0 +1,421 @@
+#define F_CPU 7372800UL
+
+#include <avr/cpufunc.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <string.h>
+#include "lcd_functions.h"
+
+#define HOMESCREEN 0
+#define NEW_PLAYER 1
+#define CHOOSE_PLAYER 2
+#define LEADERBOARDS 3
+#define GAME 4
+
+#define HS_S_Y 10 //homescreen starting y pos
+#define HS_S_X 20
+#define HS_H 60 //height
+#define HS_W 280
+
+#define CP_Y (HS_S_Y + HS_H + BLANK_SPACE) //choose player
+#define CP_X (HS_S_X)
+#define CP_H HS_H //height
+#define CP_W HS_W
+
+#define LB_Y (HS_S_Y + 2*HS_H + 2*BLANK_SPACE) //leaderboards
+#define LB_X (HS_S_X)
+#define LB_H HS_H //height
+#define LB_W HS_W
+
+
+#define BCK_BTN_Y 10
+#define BCK_BTN_X 10
+#define BCK_BTN_H 40
+#define BCK_BTN_W 80
+
+#define BLANK_SPACE 10
+#define OK_BTN_H 40
+#define OK_BTN_W 60
+#define OK_BTN_Y (MAX_X - BLANK_SPACE - OK_BTN_H)
+#define OK_BTN_X (MAX_Y - BLANK_SPACE - OK_BTN_W)
+
+#define DEL_BTN_H 40
+#define DEL_BTN_W 60
+#define DEL_BTN_Y (MAX_X - BLANK_SPACE - DEL_BTN_H)
+#define DEL_BTN_X (MAX_Y - 2*BLANK_SPACE - DEL_BTN_W - OK_BTN_W)
+
+#define START_BTN_H 40
+#define START_BTN_W 90
+#define START_BTN_Y (MAX_X - BLANK_SPACE - START_BTN_H)
+#define START_BTN_X (MAX_Y - BLANK_SPACE - START_BTN_W)
+
+#define SP_BTN_Y 70 //show player rect
+#define SP_BTN_X 20
+#define SP_BTN_H 40
+#define SP_BTN_W 120
+
+#define KEY_Y 70 //key od keyboard
+#define KEY_X 10
+#define KEY_H 30
+#define KEY_W 30
+
+#define CHAR_H 8 //char height
+#define CHAR_W 5 //char width
+#define FONT_SIZE 3
+
+char newPlayerName[7] = {' '}; //6+nulterm
+//newPlayerName[6] = '\0';
+uint8_t newPlayerNameIndex = 0;
+struct Player {
+	//Player() {
+	//points = 0;
+	//}
+	
+	uint8_t points;
+	uint16_t color;
+	uint8_t clicked;
+	char name[7]; //6 + nulterm
+};
+Player players[4];
+uint8_t players_size = 4; //stavi u 0 poslije
+//uint8_t players_size = 0; //stavi u 0 poslije
+uint8_t cross_chosen = 5, nought_chosen = 5; //5 je kao nista nije odabrano, ne mozes -1 jer ne unsigned int
+uint8_t hs_first_enter = 0; //home screen
+
+
+void print_keyboard(char str[]) {
+	uint8_t offset_x, offset_y;
+	//char *str = "ABCDEFGHIJKLMNJOPRSTUVZ";
+	//char str[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Z'};
+	offset_x = (KEY_W - 1 * CHAR_W * FONT_SIZE) / 2;
+	offset_y = (KEY_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	char tmp[2];
+	for(uint8_t i = 0; i < strlen(str); i++) { 
+		sprintf(tmp, "%c\0", str[i]);
+		draw_rectangle(KEY_Y + i/9 * (KEY_H + BLANK_SPACE/2), KEY_X + i%9*(BLANK_SPACE/2 + KEY_W), KEY_H, KEY_W, WHITE);
+		print_string(KEY_Y + offset_y + i/9 * (KEY_H + BLANK_SPACE / 2), KEY_X + i%9*(KEY_W + BLANK_SPACE / 2) + offset_x, 3, WHITE, CYAN, tmp);
+	}
+}
+
+
+void drawHomeScreen() {
+	uint8_t offset_x, offset_y;
+	
+	offset_x = (HS_W - strlen("NEW PLAYER") * CHAR_W * FONT_SIZE) / 2;
+	offset_y = (HS_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	draw_rectangle(HS_S_Y, HS_S_X, HS_H, HS_W, WHITE);
+	print_string(HS_S_Y + offset_y, HS_S_X + offset_x, 3, WHITE, CYAN, "NEW PLAYER\0");
+	
+	offset_x = (HS_W - strlen("CHOOSE PLAYER") * CHAR_W * FONT_SIZE) / 2;
+	offset_y = (HS_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	draw_rectangle(CP_Y, CP_X, CP_H, CP_W, WHITE);
+	print_string(CP_Y + offset_y, CP_X + offset_x, 3, WHITE, CYAN, "CHOOSE PLAYER\0");
+	
+	offset_x = (HS_W - strlen("LEADERBOARDS") * CHAR_W * FONT_SIZE) / 2;
+	offset_y = (HS_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	draw_rectangle(LB_Y, LB_X, LB_H, LB_W, WHITE);
+	print_string(LB_Y + offset_y, LB_X + offset_x, 3, WHITE, CYAN, "LEADERBOARDS\0");
+}
+
+void drawBackButton() {
+	uint8_t offset_x, offset_y;
+	
+	offset_x = (BCK_BTN_W - strlen("BACK") * CHAR_W * FONT_SIZE) / 2;
+	offset_y = (BCK_BTN_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	draw_rectangle(BCK_BTN_Y, BCK_BTN_X, BCK_BTN_H, BCK_BTN_W, WHITE);
+	print_string(BCK_BTN_Y + offset_y, BCK_BTN_X + offset_x, 3, WHITE, CYAN, "BACK\0");
+}
+
+void clrScr() {
+	set_background_color(CYAN);
+}
+
+void resetNewPlayerName() {
+	for(uint8_t i = 0; i < strlen(newPlayerName); i++) {
+		newPlayerName[i] = ' ';
+	}
+	newPlayerNameIndex = 0;
+}
+
+void resetPlayersColors() { //andClicks,, mozda bolje nazvat funkciju
+	for(uint8_t i = 0; i < players_size; i++) {
+		players[i].color = WHITE;
+		players[i].clicked = 0;
+	}
+	cross_chosen = 5;
+	nought_chosen = 5;
+}
+void checkBackButtonPressed(uint16_t *TP_X, uint16_t *TP_Y, uint8_t *currentPage) {
+	if(get_bit(PINB, T_IRQ) == 0) {
+		read_touch_coords(TP_X, TP_Y);
+		if(check_touch(*TP_X, *TP_Y, BCK_BTN_Y, BCK_BTN_X, BCK_BTN_H, BCK_BTN_W)) { //BACK button, ovo bolje napravit preko define-a --jesam
+			//clrScr();
+			//_delay_ms(10);
+			if(*currentPage == NEW_PLAYER) { //ako je bio u NEW PLAYER da cleara
+				resetNewPlayerName();
+			}else if(*currentPage == CHOOSE_PLAYER || *currentPage == GAME) { //ako je bio u CHOOSE PLAYER ili u GAME
+				resetPlayersColors();
+			}
+			*currentPage = HOMESCREEN;
+			//clrScr();
+		}
+	}
+}
+void sortByPoints() {
+	for(uint8_t i = 0; i < players_size - 1; i++) {
+		for(uint8_t j = 0; j < players_size - i - 1; j++) {
+			if(players[j].points < players[j+1].points) {
+				Player tmp = players[j];
+				players[j] = players[j+1];
+				players[j+1] = tmp;
+			}
+		}
+	}
+}
+
+void printLeaderboards(Player *players, uint8_t players_size) { //treba da sortira silazno po points i ovo da ljepse ispisuje
+	sortByPoints();
+	print_string(30, 100, 3, WHITE, CYAN, "NAME\0");
+	print_string(30, 100+100, 3, WHITE, CYAN, "POINTS\0");
+	for(uint8_t i = 0; i < players_size; i++) {
+		tmp[0] = players[i].points + '0';
+		print_string(80 + i*40, 100, 3, WHITE, CYAN, players[i].name);
+		print_string(80 + i*40, 100+150, 3, WHITE, CYAN, tmp);
+	}
+}
+
+void showPlayers(Player *players, uint8_t players_size) { //dodat da se imena ispisu u sredini i kad kliknes na neko ime ako je bijelo neka postane crveno ako opet kliknes na isto da postane opet bijelo, kad kliknes na neko drugo (a jedno je vec crveno) to drugo postane zeleno itd..--jesam
+	uint8_t tmp = 0, offset_x, offset_y;
+	for(uint8_t i = 0; i < players_size; i++) {
+		tmp = i > 1 ? (SP_BTN_H + BLANK_SPACE) : 0; //da ide u red ispod
+		offset_x = (SP_BTN_W - strlen(players[i].name) * CHAR_W * FONT_SIZE)/2;
+		offset_y = (SP_BTN_H - 1 * CHAR_H * FONT_SIZE)/2;
+		
+		draw_rectangle(SP_BTN_Y + tmp, SP_BTN_X + (i%2)*(SP_BTN_W + BLANK_SPACE), SP_BTN_H, SP_BTN_W, players[i].color); //i/2 jer ide i+=2
+		print_string(SP_BTN_Y + tmp + offset_y, SP_BTN_X + offset_x + (i%2)*(SP_BTN_W + BLANK_SPACE), 3, players[i].color, CYAN, players[i].name);
+	}
+}
+
+void drawOKButton() {
+	uint8_t offset_x, offset_y;
+	
+	offset_x = (OK_BTN_W - strlen("OK") * CHAR_W * FONT_SIZE) / 2;
+	offset_y = (OK_BTN_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	draw_rectangle(OK_BTN_Y, OK_BTN_X, OK_BTN_H, OK_BTN_W, WHITE);
+	print_string(OK_BTN_Y + offset_y, OK_BTN_X + offset_x, 3, WHITE, CYAN, "OK\0");
+}
+
+void drawDeleteButton() {
+	uint8_t offset_x, offset_y;
+	
+	offset_x = (DEL_BTN_W - strlen("DEL") * CHAR_W * FONT_SIZE) / 2;
+	offset_y = (DEL_BTN_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	draw_rectangle(DEL_BTN_Y, DEL_BTN_X, DEL_BTN_H, DEL_BTN_W, WHITE);
+	print_string(DEL_BTN_Y + offset_y, DEL_BTN_X + offset_x, 3, WHITE, CYAN, "DEL\0");
+}
+
+void drawStartButton() {
+	uint8_t offset_x = (START_BTN_W - strlen("START") * CHAR_W * FONT_SIZE) / 2;
+	uint8_t offset_y = (START_BTN_H - 1 * CHAR_H * FONT_SIZE) / 2;
+	draw_rectangle(START_BTN_Y, START_BTN_X, START_BTN_H, START_BTN_W, WHITE);
+	print_string(START_BTN_Y + offset_y, START_BTN_X + offset_x , 3, WHITE, CYAN, "START\0");
+}
+
+int main() {
+	TFT_init();
+
+	//initialize_menu();
+
+	TFT_start();
+
+	//uint8_t board[3][3];            // grid
+	uint16_t TP_X;                  // received coordiates rom tuch part of screen
+	uint16_t TP_Y;                  // received coordiates rom tuch part of screen
+	set_background_color(CYAN); //kao neki clearscr
+	//uint8_t i = 0;
+	uint8_t currentPage = HOMESCREEN; //0 - homescreen //vratit na HOMESCREEN
+	char str[22] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'Z'};
+	char tmp[1];
+	//print_keyboard(str);
+	//Player players[4];
+	
+	Player p1; //ovo dinamicki radit, ovo je samo radi probavanja
+	p1.points = 0;
+	for(uint8_t j = 0; j < 7; j++) {
+		p1.name[j] = 'A';
+	}
+	p1.name[6] = '\0';
+	p1.clicked = 0;
+	p1.color = WHITE;
+	
+	Player p2;
+	p2.points = 2;
+	for(uint8_t j = 0; j < 7; j++) {
+		p2.name[j] = 'B';
+	}
+	p2.name[6] = '\0';
+	p2.clicked = 1;
+	p2.color = WHITE;
+	
+	Player p3;
+	p3.points = 7;
+	for(uint8_t j = 0; j < 7; j++) {
+		p3.name[j] = 'C';
+	}
+	p3.name[6] = '\0';
+	p3.clicked = 0;
+	p3.color = WHITE;
+	
+	Player p4;
+	p4.points = 4;
+	for(uint8_t j = 0; j < 7; j++) {
+		p4.name[j] = 'D';
+	}
+	p4.name[6] = '\0';
+	p4.clicked = 0;
+	p4.color = WHITE;
+	
+	players[0] = p1;
+	players[1] = p2;
+	players[2] = p3;
+	players[3] = p4;
+	
+	while (1) {
+		// if screen is touched
+		if(currentPage == HOMESCREEN) {
+			//clrScr();
+			if(hs_first_enter) { //bez ovoga ostanu stvari od proslog page-a nez zas iako sam gore u back buttonu stavio clrScr()
+				clrScr();
+				hs_first_enter = 0;
+			}
+			drawHomeScreen();
+			if(get_bit(PINB, T_IRQ) == 0) {
+				read_touch_coords(&TP_X, &TP_Y);
+				if(check_touch(TP_X, TP_Y, HS_S_Y, HS_S_X, HS_H, HS_W)) { //NEW_PLAYER
+					clrScr();
+					hs_first_enter = 1;
+					currentPage = NEW_PLAYER;
+				}
+				if(check_touch(TP_X, TP_Y, CP_Y, CP_X, CP_H, CP_W)) { //CHOOSE_PLAYER
+					clrScr();
+					hs_first_enter = 1;
+					currentPage = CHOOSE_PLAYER;
+				}
+				if(check_touch(TP_X, TP_Y, LB_Y, LB_X, LB_H, LB_W)) { //LEADERBOARDS
+					clrScr(); //tu da se clear-a da se stalno ne re-cleara
+					hs_first_enter = 1;
+					currentPage = LEADERBOARDS;
+				}
+			}
+		}else if(currentPage == NEW_PLAYER) {
+			drawBackButton();
+			print_keyboard(str);
+			checkBackButtonPressed(&TP_X, &TP_Y, &currentPage);
+			drawDeleteButton();
+			drawOKButton();
+				
+			if(get_bit(PINB, T_IRQ) == 0) {
+				read_touch_coords(&TP_X, &TP_Y);
+				for(uint8_t i = 0; i < strlen(str); i++){
+					if(check_touch(TP_X, TP_Y, KEY_Y + i/9 * (KEY_H + BLANK_SPACE/2), KEY_X + i%9*(BLANK_SPACE/2 + KEY_W), KEY_H, KEY_W)) { //stisnut neki key od keyboarda
+						if(newPlayerNameIndex == 6) break; //jer ime ima max 7 char-a
+						clrScr();
+						newPlayerName[newPlayerNameIndex] = str[i];
+						newPlayerName[newPlayerNameIndex+1] = '\0';
+						newPlayerNameIndex++;
+						print_string(200, 10, 3, WHITE, CYAN, newPlayerName);
+						//_delay_ms(500); //da ne napravi previse ocitanja
+						break;
+					}
+				}
+				if(check_touch(TP_X, TP_Y, DEL_BTN_Y, DEL_BTN_X, DEL_BTN_H, DEL_BTN_W)) {
+					if(newPlayerNameIndex > 1) { //znaci da ima jedno slovo, npr A\0
+						newPlayerNameIndex--;
+						newPlayerName[newPlayerNameIndex] = '\0';
+						clrScr();
+						print_string(200, 10, 3, WHITE, CYAN, newPlayerName);
+					}else if(newPlayerNameIndex == 1){ //nema ni jedno slovo, stavim razmak jer inace ako je nulterm bude zbugano
+						newPlayerNameIndex--;
+						newPlayerName[newPlayerNameIndex] = ' ';
+						clrScr();
+						print_string(200, 10, 3, WHITE, CYAN, newPlayerName);
+					}
+					
+				}
+				
+				if(check_touch(TP_X, TP_Y, OK_BTN_Y, OK_BTN_X, OK_BTN_H, OK_BTN_W)) { //OK button
+					//create player i da se vrati na homescreen
+					if(players_size == 4 || !newPlayerNameIndex) continue; //ako je 4 igraca ili ako nista nije unio kao ime
+					Player newPlayer;
+					//newPlayer.name = newPlayerName;
+					for(uint8_t i = 0; i < strlen(newPlayerName); i++) { //morat ces reset-at newPlayerName i to --jesam
+						newPlayer.name[i] = newPlayerName[i];
+					}
+					newPlayer.name[newPlayerNameIndex] = '\0';
+					newPlayer.points = 0;
+					newPlayer.clicked = 0;
+					newPlayer.color = WHITE;
+					players[players_size] = newPlayer;
+					players_size++;
+					resetNewPlayerName();
+					clrScr();
+					currentPage = HOMESCREEN;
+				}
+			}
+			
+		}else if(currentPage == CHOOSE_PLAYER) {
+			drawBackButton();
+			checkBackButtonPressed(&TP_X, &TP_Y, &currentPage);
+			showPlayers(players, players_size);
+			drawStartButton();
+			
+			if (get_bit(PINB, T_IRQ) == 0) {
+				read_touch_coords(&TP_X, &TP_Y);
+				uint8_t tmp;
+				for(uint8_t i = 0; i < players_size; i++) {
+					tmp = i > 1 ? 50 : 0;
+					if(check_touch(TP_X, TP_Y, SP_BTN_Y + tmp, SP_BTN_X + (i%2)*(SP_BTN_W + BLANK_SPACE), SP_BTN_H, SP_BTN_W)) { //skuzi zasto poboja cijeli stupac ako gornjeg stisnes, a nista ako donjeg --jer ti je tmp bio izvan petlje
+						if(players[i].color == WHITE && cross_chosen == 5) {
+							players[i].color = RED;
+							players[i].clicked = 1;
+							cross_chosen = i;
+						}else if(players[i].color == WHITE && cross_chosen != 5 && nought_chosen == 5) {
+							players[i].color = GREEN;
+							players[i].clicked = 1;
+							nought_chosen = i;
+						}else if(players[i].color == RED) {
+							players[i].color = WHITE;
+							cross_chosen = 5;
+							players[i].clicked = 0;
+						}else if(players[i].color == GREEN) {
+							players[i].color = WHITE;
+							nought_chosen = 5;
+							players[i].clicked = 0;
+						}
+						_delay_ms(100); //da ne napravi 2 ocitanja
+						break;
+					}
+				}
+				
+				if(check_touch(TP_X, TP_Y, START_BTN_Y, START_BTN_X, START_BTN_H, START_BTN_W)) {
+					if(cross_chosen != 5 && nought_chosen != 5){ //ne moze uc u game ako nije odabrao dvojicu
+						clrScr();
+						currentPage = GAME;
+					}
+				}
+			}
+		}else if(currentPage == LEADERBOARDS) {
+			drawBackButton();
+			checkBackButtonPressed(&TP_X, &TP_Y, &currentPage);
+			//printLeaderboards(players, sizeof(players) / sizeof(players[0])); //moras prije nego saljes u funkciju jer se u funkciju salje samo pointer, ali ovo ce ti poslat za koliko njih je alocirano mjesta, a ne koliko ih je stvarno unutra
+			printLeaderboards(players, players_size);
+		}else if(currentPage == GAME) {
+			//dodat kod za igru
+			drawBackButton();
+			checkBackButtonPressed(&TP_X, &TP_Y, &currentPage);
+			print_string(200, 200, 3, WHITE, CYAN, "BOK\0"); //maknut
+		}
+		
+	}
+}
